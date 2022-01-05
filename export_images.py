c5815d33c8db57e77c9eff8165d4836334da89c2
Shatur <genaloner@gmail.com>
2022-01-05 17:04:07 +0200

    Update export_images.py for Krita 5.0

--- export_images.py
+++ export_images.py
@@ -5,17 +5,19 @@ from pathlib import Path
 
 def export_file(filename, output_path):
     document = Krita.instance().openDocument(str(filename))
+    output_path.mkdir(parents=True, exist_ok=True)
 
     # Resize the image if it contains a special suffix.
     fixed_filename, separator, size_suffix = filename.stem.rpartition('-')
     if size_suffix:
         filename = Path(filename.parent, fixed_filename + filename.suffix)
         x, separator, y, = size_suffix.partition('x')
-        document.scaleImage(int(x), int(y), document.xRes(), document.yRes(), 'Hermite')
+        document.scaleImage(int(x), int(y), int(document.xRes()), int(document.yRes()), 'Hermite')
 
+    output_filename = str(output_path / filename.with_suffix('.png'))
     document.setBatchmode(True)
-    document.saveAs(str(output_path / filename.with_suffix('.png')))
-    print(output_path / filename.with_suffix('.png'))
+    document.saveAs(output_filename)
+    print(f'Saved to {output_filename}')
     document.close()
 
 
